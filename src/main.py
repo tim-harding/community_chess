45b51dadbe002fee6cd6b2a917cc4a8347bb2c95
Tim Harding <tim@timharding.co>
2022-09-22 12:12:50 -0700

    Created a test post

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 import os
 import praw
 
-username = os.environ["COMMUNITY_CHESS_USERNAME"],
-reddit = praw.Reddit(
-    client_id=os.environ["COMMUNITY_CHESS_ID"],
-    client_secret=os.environ["COMMUNITY_CHESS_SECRET"],
-    password=os.environ["COMMUNITY_CHESS_PASSWORD"],
-    user_agent=f"Community Chess (by u/{username})",
-    username=username,
-)
+
+def main():
+    username = os.environ["COMMUNITY_CHESS_USERNAME"],
+    # TODO(tim-harding): Use praw.ini instead of environment variables
+    # https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html
+    reddit = praw.Reddit(
+        client_id=os.environ["COMMUNITY_CHESS_ID"],
+        client_secret=os.environ["COMMUNITY_CHESS_SECRET"],
+        password=os.environ["COMMUNITY_CHESS_PASSWORD"],
+        user_agent=f"Community Chess (by u/{username})",
+        username=username,
+    )
+    subreddit = reddit.subreddit("CommunityChess")
+    subreddit.submit("Test", selftext="Self text")
+
+
+if __name__ == "__main__":
+    main()
