94c632610c74c0c31522f1f667fd821c48176d9f
Cameron <evansc26@wwu.edu>
2022-09-29 14:19:07 -0700

    added praw.ini support

--- src/main.py
+++ src/main.py
@@ -3,18 +3,10 @@ import praw
 
 
 def main():
-    username = os.environ["COMMUNITY_CHESS_USERNAME"],
-    # TODO(tim-harding): Use praw.ini instead of environment variables
-    # https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html
-    reddit = praw.Reddit(
-        client_id=os.environ["COMMUNITY_CHESS_ID"],
-        client_secret=os.environ["COMMUNITY_CHESS_SECRET"],
-        password=os.environ["COMMUNITY_CHESS_PASSWORD"],
-        user_agent=f"Community Chess (by u/{username})",
-        username=username,
-    )
+    reddit = praw.Reddit("bot1", config_interpolation="basic")
     subreddit = reddit.subreddit("CommunityChess")
-    subreddit.submit("Test", selftext="Self text")
+    #subreddit.submit("Test", selftext="Self text")
+    print(reddit.user.me())
 
 
 if __name__ == "__main__":
