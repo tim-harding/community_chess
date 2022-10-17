924661a3c1f16600c4f7dbdb5a5cb4c6c29d604b
Cameron <evansc26@wwu.edu>
2022-10-17 12:48:45 -0700

    control script

--- 24hr.bash
+++ 24hr.bash
@@ -2,3 +2,6 @@
 #This script will:
     #Kill the old responseDaemon process
     #Launch main.py which should make a new post and fork a new responseDaemon.py to run for the next 24 hours
+
+killall python3
+python3 ~/main.py
