3d4473c0b9a96815471a5c4d3400b7e0688a7656
Tim Harding <tim@timharding.co>
2023-12-18 20:16:30 -0800

    Title reflects game result

--- src/main.py
+++ src/main.py
@@ -10,8 +10,15 @@ import database
 def main():
     database.prepare()
     database.insert_game()
-    database.insert_move("e4")
+    database.insert_move("f3")
     database.insert_move("e5")
+    database.insert_move("g4")
+    # database.insert_move("Qh4")
+    # database.insert_move("d4")
+    # database.insert_move("f6")
+    # database.insert_move("e4")
+    # database.insert_move("g5")
+    # database.insert_move("Qh5")
     make_post()
 
 
@@ -23,9 +30,19 @@ def make_post():
     svg = chess.svg.board(board, size=1024)
     cairosvg.svg2png(svg, write_to=path)
 
+    title = None
+    if board.result() == "1-0":
+        title = "Black wins"
+    elif board.result() == "0-1":
+        title = "White wins"
+    elif board.result() == "*":
+        move_number = board.ply() // 2 + 1
+        to_play = "white" if board.ply() % 2 == 0 else "black"
+        title = f"Move {move_number}, {to_play} to play"
+
     reddit = praw.Reddit()
     sub = reddit.subreddit("communitychess")
-    sub.submit_image("Test", path)
+    sub.submit_image(title, path)
 
 
 if __name__ == "__main__":
