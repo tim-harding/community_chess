255448098671e294b4ae5530804d2138b78bc79c
Tim Harding <tim@timharding.co>
2023-12-18 18:45:53 -0800

    Persisting games in the database

--- src/main.py
+++ src/main.py
@@ -1,28 +1,49 @@
 import praw
 import praw.models
 import sqlite3
+from dataclasses import dataclass
+
+
+DB = sqlite3.connect("communitychess.db")
+
+
+@dataclass
+class Move:
+    number: int
+    san: str
+    url: str
 
 
 def main():
     prepare_database()
+    print(start_game())
+
+
+def print_latest_post():
     reddit = praw.Reddit()
     sub = reddit.subreddit("communitychess")
+    for post in sub.new(limit=1):
+        print(post.title)
 
 
-def db_cursor():
-    con = sqlite3.connect("communitychess.db")
-    return con.cursor()
+def start_game():
+    cur = DB.cursor()
+    cur.execute("INSERT INTO game DEFAULT VALUES")
+    res = cur.execute("SELECT MAX(id) FROM game")
+    (id,) = res.fetchone()
+    DB.commit()
+    return id
 
 
 def prepare_database():
-    cur = db_cursor()
+    cur = DB.cursor()
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS move(
-            number INTEGER,
-            san TEXT,
-            submission_url TEXT,
-            game INTEGER,
+            number INTEGER NOT NULL,
+            san TEXT NOT NULL,
+            url TEXT NOT NULL,
+            game INTEGER NOT NULL,
             FOREIGN KEY(game) REFERENCES game(id)
         )
         """
@@ -30,10 +51,11 @@ def prepare_database():
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS game(
-            id INTEGER PRIMARY KEY
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
         )
         """
     )
+    DB.commit()
 
 
 if __name__ == "__main__":
