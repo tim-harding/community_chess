8cbb81ff883c7878523a5dd7526b980b50b2df4a
Tim Harding <tim@timharding.co>
2023-12-20 20:25:38 -0800

    Remove bash script

--- 24hr.bash
+++
@@ -1,7 +0,0 @@
-#Use crontab to launch this script as root every 24 hours
-#This script will:
-    #Kill the old responseDaemon process
-    #Launch main.py which should make a new post and fork a new responseDaemon.py to run for the next 24 hours
-
-killall python3
-python3 ~/main.py
